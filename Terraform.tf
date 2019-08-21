713d97e858e82cf53a0009fb3f964e02d72c4c33
Johanes Glenn Widiputra P <johanes.glenn@gmail.com>
2019-08-21 17:42:33 +0700

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -148,16 +148,6 @@ resource "aws_security_group" "secGroupWorkshopMYSQL" {
     security_groups = ["${aws_security_group.secGroupWorkshopWebSSH.id}"]
   }
 
-  ingress {
-    # TLS (change to whatever ports you need)
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port       = 0
     to_port         = 0
@@ -228,22 +218,22 @@ resource "aws_instance" "ec2WorkshopWebApp" {
 }
 
 
-resource "aws_instance" "ec2WorkshopWebApp2" {
-    //aws AMI selection -- Amazon Linux 2
-  ami           = "ami-0602ae7e6b9191aea"
+# resource "aws_instance" "ec2WorkshopWebApp2" {
+#     //aws AMI selection -- Amazon Linux 2
+#   ami           = "ami-0602ae7e6b9191aea"
 
-    //aws EC2 instance type, t2.micro for free tier
-  instance_type                 = "t2.micro"
-  key_name                      = "testkeypair"
-  subnet_id                     = "${aws_subnet.workshopPublicSubnet.id}"
-  vpc_security_group_ids        = ["${aws_security_group.secGroupWorkshopWebSSH.id}"]
-  //user_data_base64            = "${base64encode(var.userdataEC2)}"
-  user_data                     = "${var.userdataEC2}"
-  iam_instance_profile          =  "${aws_iam_instance_profile.instanceProfileWorkshop.name}"
-  tags = {
-    Name = "ec2WorkshopWebApp2"
-  }
-}
+#     //aws EC2 instance type, t2.micro for free tier
+#   instance_type                 = "t2.micro"
+#   key_name                      = "testkeypair"
+#   subnet_id                     = "${aws_subnet.workshopPublicSubnet.id}"
+#   vpc_security_group_ids        = ["${aws_security_group.secGroupWorkshopWebSSH.id}"]
+#   //user_data_base64            = "${base64encode(var.userdataEC2)}"
+#   user_data                     = "${var.userdataEC2}"
+#   iam_instance_profile          =  "${aws_iam_instance_profile.instanceProfileWorkshop.name}"
+#   tags = {
+#     Name = "ec2WorkshopWebApp2"
+#   }
+# }
 
 
 // create DB Subnet Group -- Subnet1+Subnet2
@@ -257,24 +247,24 @@ resource "aws_db_subnet_group" "dbSubnetGroupWorkshop" {
 }
 
 
-# resource "aws_db_instance" "rdsWorkshop" {
-#   allocated_storage         = 20
-#   storage_type              = "gp2"
-#   engine                    = "mysql"
-#   engine_version            = "5.7"
-#   instance_class            = "db.t2.micro"
-#   name                      = "rdsWorkshop"
-#   username                  = "alevz"
-#   password                  = "Passw0rdDB"
-#   vpc_security_group_ids    = ["${aws_security_group.secGroupWorkshopMYSQL.id}"]
-#   db_subnet_group_name      = "${aws_db_subnet_group.dbSubnetGroupWorkshop.tags.Name}"
-#   parameter_group_name      = "default.mysql5.7"
-#   backup_retention_period   = 1
-#   //snapshot_identifier = "some-snap"
-#   skip_final_snapshot = true
-#   publicly_accessible = true
-#   multi_az            = true
-# }
+resource "aws_db_instance" "rdsWorkshop" {
+  allocated_storage         = 20
+  storage_type              = "gp2"
+  engine                    = "mysql"
+  engine_version            = "5.7"
+  instance_class            = "db.t2.micro"
+  name                      = "rdsWorkshop"
+  username                  = "alevz"
+  password                  = "Passw0rdDB"
+  vpc_security_group_ids    = ["${aws_security_group.secGroupWorkshopMYSQL.id}"]
+  db_subnet_group_name      = "${aws_db_subnet_group.dbSubnetGroupWorkshop.tags.Name}"
+  parameter_group_name      = "default.mysql5.7"
+  backup_retention_period   = 1
+  //snapshot_identifier = "some-snap"
+  skip_final_snapshot = true
+  publicly_accessible = true
+  //multi_az            = true
+}
 
 # resource "aws_db_instance" "rdsWorkshopReplica" {
 #   allocated_storage         = 20
@@ -303,9 +293,9 @@ output "ip2" {
   value = "${aws_instance.ec2WorkshopWebApp2.public_ip}"
 }
 
-# output "ipDB"{
-#     value = "${aws_db_instance.rdsWorkshop.address}"
-# }
+output "ipDB"{
+    value = "${aws_db_instance.rdsWorkshop.address}"
+}
 
 # output "ipDBReplica"{
 #     value = "${aws_db_instance.rdsWorkshopReplica.address}"
