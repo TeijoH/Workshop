59bb14cf1c8da387dac6d2b2662dabe11a82ca29
Teijo Heimola <teijo.heimola@gofore.com>
2020-06-11 17:29:46 +0300

    Test infra edits

---
+++ demobuild/variables.tf
@@ -0,0 +1,43 @@
+variable "aws_region" {
+  description = "Region for the VPC"
+  default     = "eu-west-1"
+}
+
+variable "keypair" {
+  description = "Keypair for instances"
+  default     = "IrelandKeyPair"
+}
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for the public subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR for the private subnet"
+  default     = "10.0.2.0/24"
+}
+
+variable "availability_zones" {
+  default = [
+    "eu-west-1a",
+    "eu-west-1b",
+    "eu-west-1c",
+  ]
+  type = "list"
+}
+
+variable "ami" {
+  description = "AMI for EC2"
+  default     = "ami-0ea3405d2d2522162"
+}
+
+
+variable "key_path" {
+  description = "SSH Public Key path"
+  default     = "~/.ssh/gitreader.pem.pub"
+}
