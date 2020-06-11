6d12ed0515a6603ae67c41d235e58868d71fda2b
Teijo Heimola <teijo.heimola@gofore.com>
2020-06-11 19:27:15 +0300

    some changes

--- demobuild/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "${var.statebucket}"
-    key            = "${var.statekey}"
-    region         = "${var.aws_region}"
-    dynamodb_table = "${var.dynamostatetable}"
-  }
-}
